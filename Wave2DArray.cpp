37aa61347624ef5a19a57bbf307615935fe2a601
Vansh Johari <71847743+VanshJohari29@users.noreply.github.com>
2022-02-08 01:59:30 +0530

    Create Wave2DArray.cpp

---
+++ Wave2DArray.cpp
@@ -0,0 +1,32 @@
+vector<int> wavePrint(vector<vector<int>> arr, int nRows, int mCols)
+{
+     vector<int> ans;
+
+       for(int j = 0; j<mCols; j++){
+
+       if(j%2==0){
+
+           for(int i = 0; i<nRows; i++){
+
+               ans.push_back(arr[i][j]);
+
+           }
+
+       }
+
+       else{
+
+           for(int i = nRows - 1; i>=0; i--){
+
+               ans.push_back(arr[i][j]);
+
+           }
+
+       }    
+
+   }
+
+   
+
+   return ans;
+}
