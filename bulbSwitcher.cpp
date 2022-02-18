3e7dc829530d4980ff90aa76ba968ecab39cc62c
Vansh Johari <71847743+VanshJohari29@users.noreply.github.com>
2022-02-18 12:49:56 +0530

    Create bulbSwitcher.cpp

---
+++ bulbSwitcher.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int bulbSwitch(int n) {
+        return sqrt (n);
+    }
+};
+//WE WILL FIND OUT THAT THE PATTERN THAT THE ANSWER IS THE SQRT OF N.
+// FOR 1 -> 1
+//FOR 2 -> 1
+//FOR 3-> 1
+//FOR 4-> 2
+//FOR 9-> 3
