01dddf65b38cf2e30904882f4ada65376daead8b
Vansh Johari <71847743+VanshJohari29@users.noreply.github.com>
2022-04-13 00:32:00 +0530

    Create largestPerimeterTriangle.cpp

---
+++ largestPerimeterTriangle.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int largestPerimeter(vector<int>& nums) {
+            sort(nums.begin(),nums.end());
+        int ans=0;
+        for(int i=nums.size()-1;i>=2;i--){
+            if(nums[i]<nums[i-1]+nums[i-2]){
+                ans = nums[i]+nums[i-1]+nums[i-2];
+                return ans;
+            }
+        }
+        return 0;
+    }
+};
